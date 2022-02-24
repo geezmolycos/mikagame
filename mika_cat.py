3a1ff58402361ea98f7dad0f01f6441bbce7fa52
geezmolycos <djy0212@foxmail.com>
2022-02-24 22:33:34 +0800

    增加了动态组字的旋转、镜像和权重

--- mika_cat.py
+++ mika_cat.py
@@ -1,6 +1,7 @@
 
 from mika_screen import Vector2D, List2D, Cardinal, ScreenCell
-from mika_htmlui import HTMLGameScreen
+# from mika_htmlui import HTMLGameScreen
+from mika_svgui import SVGGameScreen
 
 from styleml.core import StyleMLCoreParser, ReturnCharExtParser
 from styleml.portal_ext import PortalExtParser
@@ -13,7 +14,7 @@ from js import jQuery as jq
 
 import asyncio
 
-htmlui = HTMLGameScreen()
+ui = SVGGameScreen()
 
 styleml_parser = StyleMLCoreParser(
     ext_parser=[
@@ -27,7 +28,7 @@ styleml_parser = StyleMLCoreParser(
         ]
     )
 
-jq("body").append(htmlui.jq_cont)
+jq("body").append(ui.jq_svg)
 
 caret_pos = Vector2D(0, 0)
 highlight_pos = Vector2D(0, 0)
@@ -35,24 +36,24 @@ def cat(key, ctrl, shift, alt):
     global caret_pos, highlight_pos
     if key[:3] == "Key":
         ch = key[3] if shift else key[3].lower()
-        htmlui.print_cell(caret_pos, ScreenCell(ch))
+        ui.print_cell(caret_pos, ScreenCell(ch))
         caret_pos += Cardinal.RIGHT
     elif key[:5] == "Arrow":
         caret_pos += dict(Up=Cardinal.UP, Down=Cardinal.DOWN, Left=Cardinal.LEFT, Right=Cardinal.RIGHT)[key[5:]]
     elif key == "Backspace":
         caret_pos += Cardinal.LEFT
-        htmlui.print_cell(caret_pos, ScreenCell())
+        ui.print_cell(caret_pos, ScreenCell())
     elif key == "Enter":
         caret_pos = Vector2D(0, caret_pos.y + 1)
     elif key == "Space":
         caret_pos += Cardinal.RIGHT
     elif key == "Delete":
-        htmlui.print_cell(caret_pos, ScreenCell())
-    htmlui.paint_cell(highlight_pos, dict(hlit=False))
+        ui.print_cell(caret_pos, ScreenCell())
+    ui.paint_cell(highlight_pos, dict(hlit=False))
     highlight_pos = caret_pos
-    htmlui.paint_cell(highlight_pos, dict(hlit=True))
+    ui.paint_cell(highlight_pos, dict(hlit=True))
 
-htmlui.registered_onkeypress.append(cat)
+ui.registered_onkeypress.append(cat)
 
 interrupt_events = {}
 next_animation_id = 0
@@ -62,7 +63,7 @@ def _(e):
     tokens = styleml_parser.render(styleml_parser.transform(styleml_parser.tokenize(
         s
     )))
-    #htmlui.scr.print_footprints(footprints)
+    #ui.scr.print_footprints(footprints)
     
     global next_animation_id
     interrupt_event = asyncio.Event()
@@ -71,18 +72,20 @@ def _(e):
     next_animation_id += 1
     async def _():
         try:
-            await htmlui.async_print_tokens(tokens, origin=Vector2D(0, 0))
+            await ui.async_print_tokens(tokens, origin=Vector2D(0, 0))
             interrupt_events.pop(current_animation_id)
         except Exception as e:
             import traceback
             traceback.print_exc()
     asyncio.create_task(_())
-    # htmlui.screen_update(jq_cont)
+    # ui.screen_update(jq_cont)
 
 jq("#show-sty").on("click", create_proxy(_))
 
 def _(e):
-    htmlui.clear_screen()
+    #import cProfile
+    #cProfile.run("ui.clear_screen()")
+    ui.clear_screen()
     
 jq("#clear-sty").on("click", create_proxy(_))
 
--- mika_svgui.py
+++ mika_svgui.py
@@ -15,8 +15,8 @@ def create_svg_elem(name):
 @attr.s
 class SVGGameScreen(GameScreen):
     
-    cell_height = 24
-    cell_width = 24
+    cell_height = 96
+    cell_width = 96
     
     def __attrs_post_init__(self):
         super().__attrs_post_init__()
@@ -106,41 +106,57 @@ class SVGGameScreen(GameScreen):
     class CustomGlyphRenderers:
         
         @classmethod
-        def _c_hz_lr(cls, width, height, children):
-            each_width = width / len(children)
+        def _c_hz_lr(cls, width, height, children): # 左右结构
             combined = jq(create_svg_elem("g"))
-            for i, child in enumerate(children):
+            total_weight = sum(weight for weight, child in children)
+            ratio = 0
+            for weight, render in children:
                 r = jq(create_svg_elem("g")).attr(
                     "transform",
-                    f"translate({i * each_width}, 0) scale({1 / len(children)}, 1.0)"
-                    ).append(child)
+                    f"translate({ratio * width}, 0) scale({weight / total_weight}, 1.0)"
+                    ).append(render)
                 combined.append(r)
+                ratio += weight / total_weight
             return combined
         @classmethod
-        def _c_hz_ud(cls, width, height, children):
-            each_height = height / len(children)
+        def _c_hz_ud(cls, width, height, children): # 上下结构
             combined = jq(create_svg_elem("g"))
-            for i, child in enumerate(children):
+            total_weight = sum(weight for weight, child in children)
+            ratio = 0
+            for weight, render in children:
                 r = jq(create_svg_elem("g")).attr(
                     "transform",
-                    f"translate(0, {i * each_height}) scale(1.0, {1 / len(children)})"
-                    ).append(child)
+                    f"translate(0, {ratio * height}) scale(1.0, {weight / total_weight})"
+                    ).append(render)
                 combined.append(r)
+                ratio += weight / total_weight
             return combined
         @classmethod
-        def _c_hz_io(cls, width, height, children):
-            shrink = 0.8
+        def _c_hz_io(cls, width, height, children, shrink=0.5, x_shift=0, y_shift=0): # 包围结构（全、边半、角半）
             size = 1
             combined = jq(create_svg_elem("g"))
-            for child in children:
-                x = width * (1 - size) / 2
-                y = height * (1 - size) / 2
+            for weight, render in children:
+                size *= weight # weight表示相对于正常缩放，进一步缩放的程度（会影响之后的字）
+                x = width * (1 - size) / 2 * (x_shift + 1)
+                y = height * (1 - size) / 2 * (y_shift + 1)
                 r = jq(create_svg_elem("g")).attr(
                     "transform",
                     f"translate({x}, {y}) scale({size}, {size})"
-                    ).append(child)
-                size *= shrink
+                    ).append(render)
                 combined.append(r)
+                size *= shrink
+            return combined
+        @classmethod
+        def _c_hz_rt(cls, width, height, child, rotation=0, flip_x=False, flip_y=False): # 旋转和镜像结构
+            # 先旋转再镜像
+            weight, render = child
+            rotation *= weight
+            combined = jq(create_svg_elem("g"))
+            r = jq(create_svg_elem("g")).attr(
+                "transform",
+                f"rotate({rotation}, {width / 2}, {height / 2}) translate({width if flip_x else 0}, {height if flip_y else 0}) scale({-1 if flip_x else 1}, {-1 if flip_y else 1})"
+                ).append(render)
+            combined.append(r)
             return combined
 
         @classmethod
@@ -157,65 +173,117 @@ class SVGGameScreen(GameScreen):
             jq_text.css("font-size", f"{width}px")
             return jq_text
         
+        _c_hz_direction = {
+            "1": (-1, 1),
+            "2": (0, 1),
+            "3": (1, 1),
+            "4": (-1, 0),
+            "5": (0, 0),
+            "6": (1, 0),
+            "7": (-1, -1),
+            "8": (0, -1),
+            "9": (1, -1),
+        }
+        
+        _c_hz_shrink = {
+            "0": 10/10,
+            "1": 9/10,
+            "2": 8/10,
+            "3": 7/10,
+            "4": 6/10,
+            "5": 5/10,
+            "6": 4/10,
+            "7": 3/10,
+            "8": 2/10,
+            "9": 1/10,
+        }
+        
+        _c_hz_weight = {
+            "0": 0,
+            "1": 1/8,
+            "2": 1/4,
+            "3": 1/3,
+            "4": 1/2,
+            "5": 1,
+            "6": 2,
+            "7": 3,
+            "8": 4,
+            "9": 8,
+            "a": 1/8,
+            "b": 2/8,
+            "c": 3/8,
+            "d": 4/8,
+            "e": 5/8,
+            "f": 6/8,
+            "g": 7/8,
+            "h": 8/8,
+            "A": 1+1/8,
+            "B": 1+2/8,
+            "C": 1+3/8,
+            "D": 1+4/8,
+            "E": 1+5/8,
+            "F": 1+6/8,
+            "G": 1+7/8,
+            "H": 1+8/8,
+        }
+
+        @classmethod
+        def _c_hz_parse_combine(cls, width, height, text, cell):
+            working_stack = []
+            def get_render(it):
+                return it.get("render") or cls._c_hz_wrap_text(width, height, it.get("ch"), cell)
+            text = list(text)
+            while len(text) > 0:
+                op = text.pop()
+                if op in tuple("%&"): # 需要额外方位信息
+                    dir = working_stack.pop().get("ch")
+                    x_shift, y_shift = cls._c_hz_direction[dir]
+                    if op == "&":
+                        shrink_char = working_stack.pop().get("ch")
+                        shrink = cls._c_hz_shrink[shrink_char]
+                elif op == "$": # 设置weight
+                    weight_char = working_stack.pop().get("ch")
+                    weight = cls._c_hz_weight[weight_char]
+                    working_stack[-1]["weight"] = weight
+                if op in tuple("-|*%&"): # 多参数指令
+                    if working_stack[-1].get("ch") not in tuple("<,"): # 两种定界符，以便于使用
+                        children = [working_stack.pop(), working_stack.pop()]
+                    else:
+                        working_stack.pop()
+                        children = []
+                        while (it := working_stack.pop()).get("ch") not in tuple(">."):
+                            children.append(it)
+                    weighted_render_children = [
+                        (it.get("weight", 1), get_render(it))
+                        for it in children
+                    ]
+                    working_stack.append(dict(render=
+                        {
+                            "-": cls._c_hz_lr,
+                            "|": cls._c_hz_ud,
+                            "*": cls._c_hz_io,
+                            "%": lambda *args, **kwargs: cls._c_hz_io(*args, **dict(x_shift=x_shift, y_shift=y_shift) | kwargs),
+                            "&": lambda *args, **kwargs: cls._c_hz_io(*args, **dict(x_shift=x_shift, y_shift=y_shift, shrink=shrink) | kwargs),
+                        }[op](width, height, weighted_render_children) # 根据指示符的不同，选择不同的结构
+                    ))
+                elif op == "/": # flip
+                    axis = working_stack.pop().get("ch")
+                    it = working_stack.pop()
+                    working_stack.append(dict(render=cls._c_hz_rt(width, height, (it.get("weight", 1), get_render(it)), flip_x=axis=="x", flip_y=axis=="y")))
+                elif op == "?": # rotate
+                    it = working_stack.pop()
+                    working_stack.append(dict(render=cls._c_hz_rt(width, height, (it.get("weight", 1), get_render(it)), rotation=180)))
+                elif op in tuple("$"):
+                    pass
+                else: # character
+                    working_stack.append({"ch": op})
+            return get_render(working_stack.pop())
+        
         @classmethod
         def c_hz(cls, scr, cell, slots):
+            slots["bg"].css("fill", cell.bg)
             c = slots["custom"]
             c.empty()
-            working_stack = []
-            for op in reversed(cell.value):
-                if op == "-":
-                    working_stack.append(
-                        cls._c_hz_lr(
-                            scr.cell_width, scr.cell_height,
-                            [working_stack.pop(), working_stack.pop()]
-                        )
-                    )
-                elif op == "=":
-                    l = []
-                    while working_stack[-1] != ".":
-                        l.append(working_stack.pop())
-                    working_stack.append(
-                        cls._c_hz_lr(
-                            scr.cell_width, scr.cell_height,
-                            l
-                        )
-                    )
-                elif op == "|":
-                    working_stack.append(
-                        cls._c_hz_ud(
-                            scr.cell_width, scr.cell_height,
-                            [working_stack.pop(), working_stack.pop()]
-                        )
-                    )
-                elif op == ":":
-                    l = []
-                    while working_stack[-1] != ".":
-                        l.append(working_stack.pop())
-                    working_stack.append(
-                        cls._c_hz_ud(
-                            scr.cell_width, scr.cell_height,
-                            l
-                        )
-                    )
-                elif op == ".":
-                    working_stack.append(
-                        cls._c_hz_ud(
-                            scr.cell_width, scr.cell_height,
-                            [working_stack.pop(), working_stack.pop()]
-                        )
-                    )
-                elif op == ",":
-                    l = []
-                    while working_stack[-1] != ".":
-                        l.append(working_stack.pop())
-                    working_stack.append(
-                        cls._c_hz_ud(
-                            scr.cell_width, scr.cell_height,
-                            l
-                        )
-                    )
-                else: # character
-                    working_stack.append(cls._c_hz_wrap_text(scr.cell_width, scr.cell_height, op, cell))
-            return working_stack[-1]
-
+            combined = cls._c_hz_parse_combine(scr.cell_width, scr.cell_height, cell.ch.value, cell)
+            c.append(combined)
 
